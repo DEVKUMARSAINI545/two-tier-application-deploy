6fe0f722249ac4fb643b01e8fb75ce3d4afd786e
dev kumar saini <devsaini27806@gmail.com>
2025-08-19 16:22:07 +0530

    lastest commit

--- terraform/scripts/install.sh
+++ terraform/scripts/install.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
+
+set -e 
+
 update_system(){
+        echo "System is updating..."
         sudo apt update -y
+
 }
+
 install_requirements(){
-        sudo apt install -y docker.io
-        sudo systemctl start docker
-        sudo systemctl enable docker
-        sudo systemctl restart docker
+        echo "Installing requirements"
+        sudo apt install -y docker.io curl
+         sudo systemctl enable docker
+    sudo systemctl restart docker
+    sudo curl -SL https://github.com/docker/compose/releases/download/v2.28.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 }
-
 group_added(){
-        sudo usermod -aG docker ubuntu
-        newgrp docker
+    echo "Adding user to Docker group..."
+    sudo usermod -aG docker $USER
+    newgrp docker
 }
 
-start_file()
-{
- sudo curl -SL https://github.com/docker/compose/releases/download/v2.28.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+clone_project(){
 
-sudo mkdir -p /home/ubuntu/myapp
-cd /home/ubuntu/myapp
-
-sudo docker-compose up -d
+       REPO="two-tier-application-deploy"
+       if [  -d  "$REPO" ]; then
+       echo "Repo '$REPO' already exist . Skipping clone"
+       else 
+        echo "Cloning...."
+        git clone  https://github.com/DEVKUMARSAINI545/two-tier-application-deploy.git
+fi
 }
 
+start_application(){
+
+     APP_DIR="two-tier-application-deploy/two-tier-flask-app"
+     if [ ! -d "$APP_DIR" ]; then
+       echo "App folder not found: $APP_DIR"
+        exit 1
+fi
+           echo "Starting Docker Compose in $APP_DIR..."
+           (cd "$APP_DIR" && docker-compose up -d)
+
+}
 
 
 update_system
 install_requirements
 group_added
-start_file
+clone_project
+start_application
